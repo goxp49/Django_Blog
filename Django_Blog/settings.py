f525020f058727c394e011c1aec4d78ae8963074
goxp49 <215920363@qq.com>
2018-06-11 22:08:30 +0800

    正在修改文章数据库部分。

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Article(models.Model):
+    article_title = models.CharField(max_length=50,unique=True)
+    article_describe = models.CharField(max_length=50)
+    article_content = models.CharField(max_length=5000)
+    article_cover = models.ImageField(upload_to='cover')
+    article_ip = models.GenericIPAddressField()
+    article_author = models.OneToOneField(get_user_model(),null=True,on_delete=models.SET_NULL)
+    article_date = models.DateTimeField('last date of modify')
+    article_hits = models.IntegerField()
+
+    def __str__(self):
+        return self.article_title
+
+    def was_modify_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.article_date <= now
+
+    was_modify_recently.admin_order_field = 'article_date'
+    was_modify_recently.boolean = True
+    was_modify_recently.short_description = 'Modify recently?'
